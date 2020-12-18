44f9879a11396a98fb707476a19c7888bb166472
Adam Tranquilla <adam.tranquilla@gmail.com>
2020-12-18 05:39:39 +0000

    character counter to 140 w/ error

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   const $tweetText = $("#tweet-text"); // this is an obj
   const update = () => {
     const chars = 140 - $tweetText.val().length;
-    const $counter = $("#counter")
-    $counter.text(chars)
+    const $counter = $("#counter");
+    $counter.text(chars);
     if (chars < 0) {
       $counter.css({ color: "red" });
       $(".err").html(`<i class="fas fa-exclamation-triangle"></i>Text too long!`);
